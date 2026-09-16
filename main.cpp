c666b3f53350b5b950a9cd4a8a89760e55cf6114
RealRyanJames <realcoder2023@gmail.com>
2026-09-16 16:55:26 -0400

    REMOVED SOME UI ELEMENTS

--- main.cpp
+++ main.cpp
@@ -61,23 +61,23 @@ public:
     }
 };
 
-std::string upper(std::string str)
+static std::string upper(std::string str)
 {
     std::transform(str.begin(), str.end(), str.begin(), ::toupper);
     return str;
 }
 
- static bool isCheck()
- {
-     NumbersInit init;
-     if (init.getChecked())
-     {
-         std::cout << upper("Enter Answer: ") << std::endl;
-         return true;
-     }
+static bool isCheck()
+{
+    NumbersInit init;
+    if (init.getChecked())
+    {
+        std::cout << upper("Enter Answer: ") << std::endl;
+        return true;
+    }
 
-     return false;
- }
+    return false;
+}
 
 struct Pos
 {
@@ -85,83 +85,104 @@ struct Pos
     std::string operatorsCreated;
 };
 
- class Stdin
+class Stdin
 
- {
+{
 
- public:
-     std::string getStdinInit()
-     {
+public:
+    std::string getStdinInit()
+    {
 
-         std::string str;
-         std::cout << "Enter Operator for Math: " << std::endl;
+        std::string str;
+        std::cout << "Enter Operator for Math: " << std::endl;
 
-         std::cin >> str;
-         return str;
-     }
- };
+        std::cin >> str;
+        return str;
+    }
+};
 
- class ISUIAvalible {
-    
-     struct AppStartup {
+class ISUIAvalible {
 
-         static bool isWorkingUI;
-         int Length;
+    struct AppStartup {
 
-     };
+        static bool isWorkingUI;
+        int Length;
 
- private:
-     
-     bool isUIReady = true;
+    };
 
- public:
-        
-     AppStartup appStartup;
+private:
 
-     bool isReady() const {
+    bool isUIReady = true;
 
-         if (isUIReady) {
+public:
 
-             return AppStartup::isWorkingUI;
-         }
+    AppStartup appStartup;
 
-         return true;
+    bool isReady() const {
 
-     }
+        if (isUIReady) {
 
-     void setMessageEndConsole() {
+            return AppStartup::isWorkingUI;
+        }
 
-         std::cout << upper("Application Has Exited");
-     };
- };
+        return true;
 
- static std::string checkStdin()
- {
-     Stdin s;
-     std::string inUser = s.getStdinInit();
-     return inUser;
- }
+    }
 
- static std::string optionPicked(char personsOptionSet)
+    void setMessageEndConsole() {
+
+        std::cout << upper("Application Has Exited");
+    };
+};
+
+struct PostionsBoxFinal {
+
+    float x, y, z;
+};
+
+
+
+static std::string checkStdin()
+{
+    Stdin s;
+    std::string inUser = s.getStdinInit();
+    return inUser;
+}
+
+static void setupBoxUI(PostionsBoxFinal posInit) {
+
+
+    for (int i = 0; i < (posInit.x * posInit.y) * 2 / 1.7f; i++) {
+
+        std::cout << "-";
+    }
+
+};
+
+static std::string optionPicked(char personsOptionSet)
 {
     if (personsOptionSet == 'y' || personsOptionSet == 'Y')
     {
+        //setupBoxUI(boxUI);
         return upper("You Chose To Continue With Option [Y] | [y]");
+        //setupBoxUI(boxUI);
     }
 
     return upper("You Chose Not to Continue with the Application Please Try Later");
+    //setupBoxUI(boxUI);
 };
 
- static char checkedKeyPressed()
+static char checkedKeyPressed()
 {
 
+    //linesWindowUI();
+    std::cout << "\n";
     std::cout << "Enter Y | N: " << std::endl;
     char keyPressed = getchar();
 
     switch (keyPressed)
     {
     case 'Y':
-
         std::cout << optionPicked(keyPressed) << std::endl;
 
         break;
@@ -217,7 +238,7 @@ float divide(float a, float b)
     if (b == 0)
     {
         std::cout << "Cannot Divide by Zero" << std::endl;
-       
+
     }
 
     return a / b;
@@ -252,6 +273,8 @@ auto main() -> int
     if (inUser == "+")
     {
 
+        std::cout << "\n";
+        //linesWindowUI();
         GeneratedNumbers numbers;
         int num1 = numbers.getFirstNumber();
         int num2 = numbers.getSecondNumber();
